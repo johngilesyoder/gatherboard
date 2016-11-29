a0df752e45d7e0d530501c007c14f28cf1ed12e3
John Yoder <johngilesyoder@gmail.com>
2016-11-29 11:18:42 -0700

    responsify default page and billboards

--- wp-content/themes/gatherboard/template-contact.php
+++ wp-content/themes/gatherboard/template-contact.php
@@ -2,39 +2,36 @@
 
 <main role="main">
 	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<h1 class="page-title"><?php the_title(); ?></h1>
 
-		<h1><?php the_title(); ?></h1>
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				<!-- article -->
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php the_content(); ?>
 
-			<?php the_content(); ?>
+				</article>
+				<!-- /article -->
 
-			<?php comments_template( '', true ); // Remove if you don't want comments ?>
+			<?php endwhile; ?>
 
-			<br class="clear">
+			<?php else: ?>
 
-			<?php edit_post_link(); ?>
+				<!-- article -->
+				<article>
 
-		</article>
-		<!-- /article -->
+					<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
 
-	<?php endwhile; ?>
+				</article>
+				<!-- /article -->
 
-	<?php else: ?>
+			<?php endif; ?>
 
-		<!-- article -->
-		<article>
-
-			<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
+			</div>
+		</div>
 	</div>
 </main>
 
